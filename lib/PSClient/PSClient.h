cb9f23a84cbad0f35dd72ac7adf6544b69ede4a0
jahid <jstbyte@gmail.com>
2022-11-08 19:33:34 +0530

    PSClient an easy to use mqtt library created.

---
+++ lib/PSClient/PSClient.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <Arduino.h>
+#include <LittleFS.h>
+#define SPIFFS LittleFS
+#include <WiFiClientSecure.h>
+#include <CertStoreBearSSL.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <Helper.h>
+#include <time.h>
+
+/* Mqtt SSL/TLS CA Root Certificate */
+const char mqtt_cert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
+QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB
+CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97
+nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt
+43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P
+T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4
+gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO
+BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR
+TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw
+DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr
+hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg
+06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF
+PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
+YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
+CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
+-----END CERTIFICATE-----
+)EOF";
+
+typedef struct
+{
+    String identity;
+    String wlanSSID;
+    String wlanPASS;
+    String hostNAME;
+    String mqttHOST;
+    u32_t mqttPORT;
+} wlan_config_t;
+
+class PSClient : public PubSubClient
+{
+private:
+    u32_t wifiTimeout;
+    WiFiClient *wifiClient;
+    unsigned long long timestamp;
+    std::function<void(void)> _onTimeout;
+    std::function<void(PSClient *)> _onConnection;
+
+public:
+    void eventLoop();
+    wlan_config_t begin(String path);
+    void begin(wlan_config_t *config);
+    wlan_config_t loadWlanConfig(String path);
+    void onTimeout(std::function<void(void)> cb);
+    void onConnection(std::function<void(PSClient *)> cb);
+};
