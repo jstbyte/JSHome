29fee64d50a46af443f00269b0ba210cfeda2bda
jahid <jstbyte@gmail.com>
2022-12-29 13:13:32 +0530

    Removed dual boot. WiFi & MQTT Only.

--- src/rSonoff.cpp
+++ src/rSonoff.cpp
@@ -1,9 +1,83 @@
 #ifdef rSONOFF
-#include "rSonoff/espn.cpp.h"
-#include "rSonoff/mqtt.cpp.h"
+#include <Arduino.h>
+#include <LittleFS.h>
+#define SPIFFS LittleFS
+#include <CertStoreBearSSL.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <PubSubWiFi.h>
+#include <IRutils.h>
+#include <IRrecv.h>
+#include "Helper.h"
+#include "Sonoff.h"
+#include <time.h>
+#include "spac.h"
+
+String topicSonoff;
+String topicDevInfo;
+PubSubWiFi mqttClient;
 
 decode_results ir_result;
-IRrecv irrecv(IR_RECV_PIN);
+IRrecv irrecv(13); // D7;
+
+void sonoffire()
+{
+    String topic = String(topicSonoff.c_str());
+    topic.replace("/req/", "/res/");
+    mqttClient.publish(topic.c_str(), Sonoffe::reads().c_str());
+    DEBUG_LOG_LN("MQTT: event emitted.");
+}
+
+void onConnection(PubSubWiFi *client)
+{
+    if (client->connected())
+    {
+        client->subscribe(topicDevInfo.c_str());
+        client->subscribe(topicSonoff.c_str());
+        DEBUG_LOG_LN("MQTT: subscribed.");
+        analogWrite(LED_BUILTIN, 254);
+        sonoffire();
+        return;
+    }
+
+    digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+    char data[length + 1];
+    data[length] = '\0';
+    strncpy(data, (char *)payload, length);
+
+    DEBUG_LOG("MQTT: topic recived : ");
+    DEBUG_LOG(topic);
+    DEBUG_LOG(" payload:: ")
+    DEBUG_LOG_LN(data);
+
+    if (topicSonoff == topic)
+    {
+        return Sonoffe::writer(data);
+    }
+
+    if (topicDevInfo == topic)
+    {
+        String devInfoJsonDoc;
+        StaticJsonDocument<256> doc;
+        doc["uid"] = ESP.getChipId();
+        doc["mac"] = WiFi.macAddress();
+
+        JsonObject services = doc["services"].createNestedObject();
+        services["name"] = "sonoff";
+        services["data"] = Sonoffe::count();
+
+        serializeJson(doc, devInfoJsonDoc);
+        String topic = String(topicDevInfo.c_str()) + "/" + ESP.getChipId();
+        topic.replace("/req/", "/res/");
+        mqttClient.publish(topic.c_str(), devInfoJsonDoc.c_str());
+        return;
+    }
+}
 
 void setup()
 {
@@ -11,34 +85,19 @@ void setup()
     irrecv.enableIRIn();
     Serial.begin(115200);
     Sonoffe::load("/rsonoff.json");
-    if (BootMan::recover(Sonoffe::pins(), Sonoffe::count()))
-    {
-        auto connMan = BootMan::data();
-        connMan->timeout = WIFI_RETRY_TIMEOUT;
-        Sonoffe::writes(HIGH);
-    }
-
+    Sonoffe::writes(HIGH);
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
 
-    DEBUG_LOG("\nMQTT: & WiFi Timeout : ");
-    DEBUG_LOG_LN(BootMan::data()->timeout);
-    DEBUG_LOG("Device Mac Address : ");
-    DEBUG_LOG_LN(WiFi.macAddress());
+    auto config = mqttClient.init("/wconfig.json");
+    Sonoffe::event.setCallback(sonoffire, 500);
+    mqttClient.onConnection(onConnection);
+    mqttClient.setCallback(mqttCallback);
 
-    if (BootMan::data()->timeout)
-    {
-        BootMan::data()->timeout--;
-        mqttClient = new PubSubWiFi;
-        Sonoffe::event.setCallback(emmittMqttEvent, 500);
-        setupMqtt("/wconfig.json");
-    }
-    else
-    {
-        mqttClient = nullptr;
-        setupEspNow();
-        Sonoffe::event.setCallback(emmittEspNowEvent, 500);
-    }
+    /* Prepare Topics */
+    topicDevInfo = config.identity + "/req/devinfo";
+    topicSonoff = config.identity + "/req/sonoff/" + String(ESP.getChipId());
+    DEBUG_LOG_LN("MQTT: Topics Ready.");
 }
 
 bool ledState = true;
@@ -46,26 +105,17 @@ void loop()
 {
     if (irrecv.decode(&ir_result))
     {
+        if (!mqttClient.connected())
+            digitalWrite(LED_BUILTIN, LOW);
         DEBUG_LOG("INFARED RECIVED : ");
         Sonoffe::press(ir_result.value);
         DEBUG_LOG_LN(ir_result.value);
         irrecv.resume();
+        if (!mqttClient.connected())
+            digitalWrite(LED_BUILTIN, HIGH);
     }
 
-    if (mqttClient)
-    {
-        mqttClient->eventLoop();
-        if (ledState)
-            analogWrite(LED_BUILTIN, 255);
-        else
-            analogWrite(LED_BUILTIN, 254);
-        ledState = !ledState;
-    }
-    else
-    {
-        analogWrite(LED_BUILTIN, 254);
-    }
-
+    mqttClient.eventLoop();
     MsgPacketizer::parse();
     Sonoffe::event.loop();
     delay(100); // Balance CPU Loads;
